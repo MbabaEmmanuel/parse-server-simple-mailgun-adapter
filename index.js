9a2d5ef34dbe4c758516db1630b0bafb3d551893
EmmanuelMbaba <emmanuel.mbaba32@gmail.com>
2017-12-18 12:23:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 
-var Mailgun = require('mailgun-js');
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// Welcome Email for new users
+Parse.Cloud.define("WelcomeEmailnction(request, response) {
 
-var SimpleMailgunAdapter = mailgunOptions => {
-  if (!mailgunOptions || !mailgunOptions.apiKey || !mailgunOptions.domain || !mailgunOptions.fromAddress) {
-    throw 'SimpleMailgunAdapter requires an API Key, domain, and fromAddress.';
-  }
-  var mailgun = Mailgun(mailgunOptions);
+var Mailgun = require('mailgun');
+ Mailgun.initialize('smtp.mailgun.org', 'key-466fc3d292440647622a5096ec517b3d');
 
-  var sendMail = mail => {
-    var data = {
-      from: mailgunOptions.fromAddress,
-      to: mail.to,
-      subject: mail.subject,
-      text: mail.text,
-    }
+var mailEmail = request.params.email; <== You can get parameters this way
+//  Mailgun.sendEmail({
+//  to: "To Email <toEmail@example.com>",
+//  from: "From Email <fromEmail@example.com>",
+//  subject: "Welcome!",
+//  text: "Hello "  + ", \n Thank you for signing up."
 
-    return new Promise((resolve, reject) => {
-      mailgun.messages().send(data, (err, body) => {
-        if (typeof err !== 'undefined') {
-          reject(err);
-        }
-        resolve(body);
-      });
-    });
-  }
-
-  return Object.freeze({
-    sendMail: sendMail
-  });
-}
-
-module.exports = SimpleMailgunAdapter
+},{
+   success: function() {
+   response.success(request.params);
+   console.log("--email - success");
+   console.log(request.params);
+ },
+   error: function() {
+   console.log("--failedend email - success");
+   console.error(request.params);
+   response.error("Uhsomething went wrong");
+ }
+ }); 
+});
