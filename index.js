78f715257bd5c2c64b29148610570889c25dde7f
EmmanuelMbaba <emmanuel.mbaba32@gmail.com>
2017-12-18 18:21:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// Welcome Email for new users
-Parse.Cloud.define("WelcomeEmailnction(request, response) {
-
-var Mailgun = require('mailgun');
- Mailgun.initialize('smtp.mailgun.org', 'key-466fc3d292440647622a5096ec517b3d');
-
- var mailEmail = request.params.email; <== You can get parameters this way
-//  Mailgun.sendEmail({
-//  to: "To Email <toEmail@example.com>",
-//  from: "From Email <fromEmail@example.com>",
-//  subject: "Welcome!",
-//  text: "Hello "  + ", \n Thank you for signing up."
-
-},{
-   success: function() {
-   response.success(request.params);
-   console.log("--email - success");
-   console.log(request.params);
- },
-   error: function() {
-   console.log("--failedend email - success");
-   console.error(request.params);
-   response.error("Uhsomething went wrong");
- }
- }); 
+const resolve = require('path').resolve;
+// Note that the paths to the templates are absolute.
+var server = ParseServer({
+  ...otherOptions,
+  // Enable email verification
+  verifyUserEmails: true,
+  emailAdapter: {
+    module: 'parse-server-mailgun',
+    options: {
+      // The address that your emails come from
+      fromAddress: 'JokeKing <postmaster@sandbox8325a059a22e4ce7a0788087283b8aba.mailgun.org>',
+      // Your domain from mailgun.com
+      domain: 'smtp.mailgun.org',
+      // Your API key from mailgun.com
+      apiKey: 'key-466fc3d292440647622a5096ec517b3d',
+      // The template section
+      templates: {
+        passwordResetEmail: {
+          subject: 'Reset your password',
+          pathPlainText: resolve(__dirname, 'path/to/templates/password_reset_email.txt'),
+          pathHtml: resolve(__dirname, 'path/to/templates/password_reset_email.html'),
+          callback: (user) => { return { firstName: user.get('firstName') }}
+          // Now you can use {{firstName}} in your templates
+        },
+        verificationEmail: {
+          subject: 'Confirm your account',
+          pathPlainText: resolve(__dirname, 'path/to/templates/verification_email.txt'),
+          pathHtml: resolve(__dirname, 'path/to/templates/verification_email.html'),
+          callback: (user) => { return { firstName: user.get('firstName') }}
+          // Now you can use {{firstName}} in your templates
+        },
+        customEmailAlert: {
+          subject: 'Urgent notification!',
+          pathPlainText: resolve(__dirname, 'path/to/templates/custom_alert.txt'),
+          pathHtml: resolve(__dirname, 'path/to/templates/custom_alert.html'),
+        }
+      }
+    }
+  }
 });
